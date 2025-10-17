dd0dd3e10f9a436ee0e50ce23d2aa94e305f58d3
thaumatichthys <66721211+thaumatichthys@users.noreply.github.com>
2025-10-17 04:49:52 -0700

    readback working now!!!!

--- main.cpp
+++ main.cpp
@@ -40,24 +40,23 @@ static inline uint16_t bitreverse16(uint16_t x) {
 }
 
 void spi_write24(uint8_t address, uint16_t data) {
-    data = bitreverse16(data);
     uint8_t arr[] = {
         address & 0x7F,
         (uint8_t) (data >> 8),
         (uint8_t) (data & 0xFF)
     };
     spi_write_blocking(SPI_PORT, arr, 3);
-    
 }
 
-void test() {
+void init_spi() {
 
     uint8_t data[3]; // 24 bits
 
-    spi_init(SPI_PORT, 500000);
+    spi_init(SPI_PORT, 5000000);
     spi_set_format(SPI_PORT, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
     gpio_set_function(GPIO_SPI_MOSI, GPIO_FUNC_SPI);
     gpio_set_function(GPIO_SPI_SCK, GPIO_FUNC_SPI);
+    gpio_set_function(GPIO_LMX_MUXOUT, GPIO_FUNC_SPI);
 
     gpio_init(GPIO_SPI_LMX_CS);
     gpio_set_dir(GPIO_SPI_LMX_CS, GPIO_OUT);
@@ -73,12 +72,13 @@ void test() {
     // soft reset 
     //0b00 x 0001 xx xx x x x x x
     //0b00 1 0001 00 00 1 0 1 1 0
-    gpio_put(GPIO_SPI_LMX_CS, 0);
-    spi_write24(0, 0b0010001000010110); 
+    //gpio_put(GPIO_SPI_LMX_CS, 0);
+    //spi_write24(0, 0b0010001000010110); // MUXOUT = lock detect
+    //spi_write24(0, 0b0010001000010010); 
     //spi_write24(0, 1 << 1); 
-    gpio_put(GPIO_SPI_LMX_CS, 1);
-    sleep_ms(1);
-
+    //gpio_put(GPIO_SPI_LMX_CS, 1);
+    //sleep_ms(1);
+/*
 
     // // set PFD_DLY to setting 1:
     // gpio_put(GPIO_SPI_LMX_CS, 0);
@@ -94,7 +94,7 @@ void test() {
     spi_write24(0, 0b0010001000011100);
     gpio_put(GPIO_SPI_LMX_CS, 1);
     sleep_ms(1);
-/*
+
     // set N divider value
     uint16_t n_divider = 1000;
     gpio_put(GPIO_SPI_LMX_CS, 0);
@@ -134,6 +134,37 @@ void test() {
     sleep_ms(1);*/
 }
 
+void dump_values() {
+    gpio_put(GPIO_SPI_LMX_CS, 0);
+    spi_write24(0, 0b0010001000010000); 
+    gpio_put(GPIO_SPI_LMX_CS, 1);
+    printf("       | ");
+    for (int i = 0; i < 16; i++) {
+        printf("%2d  | ", i);
+    }
+
+    uint8_t addr = 0;
+    for (addr = 0; addr < 60; addr++) {
+        gpio_put(GPIO_SPI_LMX_CS, 0);
+        uint8_t cmd = addr | (1 << 7); // READ mode
+        uint8_t read_contents[2];
+        spi_write_blocking(SPI_PORT, &cmd, 1);
+        spi_read_blocking(SPI_PORT, 0, read_contents, 2);
+        gpio_put(GPIO_SPI_LMX_CS, 1);
+        sleep_ms(1);
+        uint16_t contents_merged = (uint16_t)read_contents[0] | ((uint16_t)read_contents[1] << 8); 
+        //printf("read values: R%d: 0x%x\n", addr, contents_merged);
+        printf("\nR%2d: = |  ", addr);
+        for (int i = 0; i < 16; i++) {
+            int bit = (contents_merged & (1 << (15 - i))) > 0;
+            printf("%d  |  ", bit);
+        }
+    }
+    printf("\n\n\n");
+
+    
+}
+
 int main() {
     
     stdio_init_all(); // for printf
@@ -147,20 +178,20 @@ int main() {
 
     gpio_put(GPIO_RGB_R, 1);
     gpio_put(GPIO_RGB_G, 1);
-    gpio_init(GPIO_LMX_MUXOUT);
-    gpio_set_dir(GPIO_LMX_MUXOUT, GPIO_IN);
+    //gpio_init(GPIO_LMX_MUXOUT);
+    //gpio_set_dir(GPIO_LMX_MUXOUT, GPIO_IN);
     
-    
-    test();
-
+    sleep_ms(100);
+    printf("he;;p wprld\n");
+    init_spi();
 
     while(1) { // rekt noob timeam
         gpio_put(25, 1);
         sleep_ms(20);
         gpio_put(25, 0);
-        sleep_ms(20);
-
-        gpio_put(GPIO_RGB_R, gpio_get(GPIO_LMX_MUXOUT));
-        gpio_put(GPIO_RGB_G, gpio_get(GPIO_LMX_MUXOUT));
+        sleep_ms(2000);
+        dump_values();
+       // gpio_put(GPIO_RGB_R, gpio_get(GPIO_LMX_MUXOUT));
+       // gpio_put(GPIO_RGB_G, gpio_get(GPIO_LMX_MUXOUT));
     }
 }
